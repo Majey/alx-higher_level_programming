95bb526dac3bcc754e50821ce1afb6145b4ab2df
Majey <kevinerick78@yahoo.com>
2022-10-03 20:56:30 +0300

    JS Warmup

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-<<<<<<< HEAD
-const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
-=======
 const multiLine = [
   'C is fun',
   'Python is cool',
@@ -12,5 +7,4 @@ const multiLine = [
 
 for (let i = 0; i < multiLine.length; i++) {
   console.log(multiLine[i]);
->>>>>>> 81e18e526ae42c7694b447cf0fe3cd061bd57dd1
 }
