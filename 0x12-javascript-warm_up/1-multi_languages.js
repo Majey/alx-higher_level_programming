12c85c532c9737335dd340ccc12a39edf4fe1433
Majey <kevinerick78@yahoo.com>
2022-10-03 20:50:20 +0300

    JS Warmup

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
-<<<<<<< HEAD
+
 console.log('C is fun');
 console.log('Python is cool');
 console.log('JavaScript is amazing');
-=======
-console.log('C is fun\nPython is cool\nJavaScript is amazing');
->>>>>>> 81e18e526ae42c7694b447cf0fe3cd061bd57dd1
+
+
+
+
+
+
+
