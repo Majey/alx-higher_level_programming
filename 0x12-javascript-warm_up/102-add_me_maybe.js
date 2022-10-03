9542f5e250026866d78d45091530db824db17b96
Majey <kevinerick78@yahoo.com>
2022-10-03 21:03:11 +0300

    JS Warmup

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
 exports.addMeMaybe = function (number, theFunction) {
-<<<<<<< HEAD
-  number++;
-  theFunction(number);
-=======
   theFunction(++number);
->>>>>>> 81e18e526ae42c7694b447cf0fe3cd061bd57dd1
 };
--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,20 +1,12 @@
 #!/usr/bin/node
-<<<<<<< HEAD
-const myObject = {
-=======
 let myObject = {
->>>>>>> 81e18e526ae42c7694b447cf0fe3cd061bd57dd1
   type: 'object',
   value: 12
 };
 console.log(myObject);
 
 myObject.incr = function () {
-<<<<<<< HEAD
-  myObject.value++;
-=======
   this.value++;
->>>>>>> 81e18e526ae42c7694b447cf0fe3cd061bd57dd1
 };
 
 myObject.incr();
